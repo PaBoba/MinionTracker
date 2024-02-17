48211c1ca621de916f64379f015f9b0be08dac89
PaBoba <140555335+PaBoba@users.noreply.github.com>
2024-02-16 23:01:57 -0800

    created index.js file

---
+++ index.js
@@ -0,0 +1,19 @@
+db = require("./db/connections");
+const appTitle = require("./lib/title");
+const { options } = require("./lib/queries/menuChoices");
+
+// Function to start the application
+function startApp() {
+  console.log(appTitle);
+  options();
+}
+
+db.promise()
+  .connect()
+  .then(() => {
+    console.log("Connected to the database.");
+    startApp();
+  })
+  .catch((err) => {
+    console.error("Error connecting to the database:", err);
+  });
